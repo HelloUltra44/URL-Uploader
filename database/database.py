2177a28d595229a36d228367a913986f816a7896
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-29 11:19:24 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -6,15 +6,20 @@ from config import Config
 
 
 class Database:
-
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.USERS
+        self.col = self.db.users
 
     def new_user(self, id):
-        return dict(id=id, thumbnail=None)
-
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            apply_caption=True,
+            upload_as_doc=False,
+            thumbnail=None,
+            caption=None
+        )
 
     async def add_user(self, id):
         user = self.new_user(id)
@@ -22,18 +27,31 @@ class Database:
 
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return True if user else False
+        return bool(user)
 
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
 
     async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+        return self.col.find({})
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
+
+    async def set_apply_caption(self, id, apply_caption):
+        await self.col.update_one({'id': id}, {'$set': {'apply_caption': apply_caption}})
+
+    async def get_apply_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('apply_caption', True)
+
+    async def set_upload_as_doc(self, id, upload_as_doc):
+        await self.col.update_one({'id': id}, {'$set': {'upload_as_doc': upload_as_doc}})
+
+    async def get_upload_as_doc(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('upload_as_doc', False)
 
     async def set_thumbnail(self, id, thumbnail):
         await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
@@ -42,8 +60,15 @@ class Database:
         user = await self.col.find_one({'id': int(id)})
         return user.get('thumbnail', None)
 
-async def AddUser(bot: Client, update: Message):
-    if not await db.is_user_exist(update.from_user.id):
-           await db.add_user(update.from_user.id)
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': id}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption', None)
+
+    async def get_user_data(self, id) -> dict:
+        user = await self.col.find_one({'id': int(id)})
+        return user or None
 
 db = Database(Config.DATABASE_URL, Config.DATABASE_NAME)
